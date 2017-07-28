52d8e9188cb7151da474bb8648496c33930d3cb6
Tictac0723 <taylorclark0723@gmail.com>
2017-07-28 15:48:37 -0500

    some styling and modal not working

---
+++ app/data/friends.js
@@ -0,0 +1,46 @@
+var users = [{
+    name: "Taylor",
+    photo: "https://giphy.com/gifs/4ZswIRaLo7i5q",
+    scores: [1,
+        1,
+        2,
+        3,
+        4,
+        5,
+        3,
+        4,
+        5,
+        2
+    ]
+}, {
+    name: "David",
+    photo: "https://media.giphy.com/media/9MFsKQ8A6HCN2/giphy.gif",
+    scores: [2,
+        2,
+        4,
+        3,
+        4,
+        1,
+        3,
+        5,
+        5,
+        2
+    ]
+}, {
+    name: "Lauren",
+    photo: "https://media.giphy.com/media/BnlQIRp9Ib2Ni/giphy.gif",
+    scores: [1, 
+
+        3,
+        2,
+        3,
+        4,
+        5,
+        1,
+        2,
+        3,
+        4
+    ]
+}];
+
+module.exports = users;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -4,43 +4,39 @@ var express = require("express");
 var path = require("path");
 var app = express();
 var PORT = 8080;
-var users = require("../data/friends.js");
+var userData = require("../data/friends.js");
 
 // Routes
 // ===========================================================
 module.exports = function(app) {
-app.get("/api/friends", function(req, res) {
-    res.json(users);
-});
-
-app.post("/api/friends", function(req, res) {
-    var closestDifference = 100;
-    var difference = 0;
-    var match;
-
-    users.forEach(function(friend) {
-        console.log(friend);
-        console.log(req.body);
-
-        difference = eval(friend.scores.map(function(num, index) {
-            return Math.abs(num - req.body.scores[index]);
-        }).join('+'));
-
-        if (difference <= closestDifference) {
-            closestDifference = difference;
-            match = friend;
-        }
+    app.get("/api/friends", function(req, res) {
+        return res.json(userData);
     });
-    
-    res.json(match);
-    users.push(req.body);
 
-    var newUser = req.body;
-    newUser.routeName = newUser.name.replace(/\s+/g, "").toLowerCase();
-    console.log(newUser);
-    users.push(newUser);
-    res.json(newUser);
 
-});
+    app.post("/api/friends", function(req, res) {
+        var newuser = req.body;
+
+        var closestDifference = 100;
+        var difference = 0;
+        var match;
+
+        userData.forEach(function(friend) {
+            console.log(friend);
+            console.log(req.body);
+
+            difference = eval(friend.scores.map(function(num, index) {
+                return Math.abs(num - req.body.scores[index]);
+            }).join('+'));
+
+            if (difference <= closestDifference) {
+                closestDifference = difference;
+                match = friend;
+            }
+        });
+        res.json(match);
+
+        userData.push(newuser)
+    })
 
 };
