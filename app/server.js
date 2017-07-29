d9c78024bab4642c90051c93fdeabc3f7f69df34
Tictac0723 <taylorclark0723@gmail.com>
2017-07-28 20:16:51 -0500

    HEROKU WORK

--- app/server.js
+++ app/server.js
@@ -8,7 +8,7 @@ var path = require("path");
 
 var app = express();
 
-var PORT = 8080;
+var port = process.env.PORT || 3000;
 
 
 
@@ -28,6 +28,6 @@ require("./routing/htmlRoutes.js")(app);
 
 
 
-app.listen(process.env.PORT || PORT, function() {
+app.listen(PORT, function() {
   console.log("App listening on PORT " + PORT);
 });
