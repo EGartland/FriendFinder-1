d55f25454e84d254034c2c34d1b7bf5b5ad93d2c
Tictac0723 <taylorclark0723@gmail.com>
2017-07-28 20:29:48 -0500

    another heroku try

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -3,7 +3,6 @@
 var express = require("express");
 var path = require("path");
 var app = express();
-var PORT = 8080;
 var userData = require("../data/friends.js");
 
 // Routes
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -3,7 +3,6 @@
 var express = require("express");
 var app = express();
 var path = require("path");
-var PORT = 8080;
 
 
 // Routes
