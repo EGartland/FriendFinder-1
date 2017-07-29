121ae6ec84cdc14b29d59b2dbd368f01f9111a1a
Tictac0723 <taylorclark0723@gmail.com>
2017-07-28 19:56:23 -0500

    IT'S ALIVE

--- app/data/friends.js
+++ app/data/friends.js
@@ -41,6 +41,86 @@ var users = [{
         3,
         4
     ]
+},
+{
+    name: "Ron",
+    photo: "https://media.giphy.com/media/m5FqYUKkTB2fe/giphy.gif",
+    scores: [2, 
+
+        4,
+        1,
+        3,
+        4,
+        2,
+        1,
+        2,
+        1,
+        5
+    ]
+},
+{
+    name: "Amy",
+    photo: "https://media.giphy.com/media/SLkDdynk5UjbW/giphy.gif",
+    scores: [1, 
+
+        3,
+        1,
+        3,
+        2,
+        5,
+        5,
+        1,
+        1,
+        2
+    ]
+},
+{
+    name: "Michael",
+    photo: "https://media.giphy.com/media/l4Ki2obCyAQS5WhFe/giphy.gif",
+    scores: [5, 
+
+        5,
+        5,
+        3,
+        1,
+        1,
+        2,
+        4,
+        1,
+        2
+    ]
+},
+{
+    name: "Mindy",
+    photo: "https://media.giphy.com/media/utZYdIUlFbehi/giphy.gif",
+    scores: [1, 
+
+        1,
+        1,
+        2,
+        4,
+        1,
+        5,
+        4,
+        5,
+        2
+    ]
+},
+{
+    name: "Tina",
+    photo: "https://media.giphy.com/media/nCpv15Q5IQ8EM/giphy.gif",
+    scores: [5, 
+
+        5,
+        2,
+        3,
+        4,
+        2,
+        5,
+        5,
+        5,
+        2
+    ]
 }];
 
 module.exports = users;
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -17,4 +17,5 @@ app.get("/survey", function(req, res) {
   res.sendFile(path.join(__dirname + "/../public/survey.html"));
 });
 
+app.use(express.static(__dirname + "/../public"));
 };
